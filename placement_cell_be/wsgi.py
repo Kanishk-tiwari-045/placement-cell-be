ca584fc6ad2a67b5bf390ea201aa202501cdaa78
kanishk <kanishktiwari11a@gmail.com>
2024-06-11 22:37:06 +0530

    custom_branch

--- placement_cell_be/wsgi.py
+++ placement_cell_be/wsgi.py
@@ -1,7 +1,16 @@
+"""
+WSGI config for placement_cell_be project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-# Set the DJANGO_SETTINGS_MODULE directly
-os.environ['DJANGO_SETTINGS_MODULE'] = 'placement_cell_be.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "placement_cell_be.settings")
 
 application = get_wsgi_application()
